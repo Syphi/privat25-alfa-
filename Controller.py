909e11bfb338b0c69b9e31b9715f5bd1c9bf9eff
Ralko Denis <ralkodenis@outlook.com>
2016-03-14 20:05:40 +0200

    comm

--- Controller.py
+++ Controller.py
@@ -1,48 +1,65 @@
 from module import *
 
-#  Edit_Method.Edit_sign(1,'+')
-class Balance:
+
+class balance:
 
     def balance_plus(self):
-        plus=0
-        i= 0
+        """
+        >>> dp = balance()
+        >>> dp.balance_plus()
+
+        """
+        plus = 0
+        i = 0
         for i in range(len(Operation_History)):
-            if Operation_History[i]['sign']=='+':
-                plus+=Operation_History[i]['summary']
+            if Operation_History[i]['sign'] == '+':
+                plus += Operation_History[i]['summary']
         return plus
 
     def balance_minus(self):
-        minus=0
+        """
+        >>> dp = balance()
+        >>> dp.balance_minus()
+
+        """
+        minus = 0
         for i in range(len(Operation_History)):
-            if Operation_History[i]['sign']=='-':
-                minus+=Operation_History[i]['summary']
+            if Operation_History[i]['sign'] == '-':
+                minus += Operation_History[i]['summary']
         return minus
 
-    def Balance_Difference(self):
+    def balance_difference(self):
+        """
+        >>> dp = balance()
+        >>> dp.balance_difference()
+
+        """
         return self.balance_plus()-self.balance_minus()
 
 
-class Edit:
+class edit:
 
-    def Edit_all(self,op_id,sign,summary,dd,mm,yy,comment):
-        Edit_Method().Edit_sign(op_id,sign)
-        Edit_Method().Edit_summary(op_id,summary)
-        Edit_Method().Edit_dd(op_id,dd)
-        Edit_Method().Edit_mm(op_id,mm)
-        Edit_Method().Edit_yy(op_id,yy)
-        Edit_Method().Edit_comment(op_id,comment)
+    def edit_all(self, op_id, sign, summary, dd, mm, yy, comment):
+        edit_method().edit_sign(op_id, sign)
+        edit_method().edit_summary(op_id, summary)
+        edit_method().edit_dd(op_id, dd)
+        edit_method().edit_mm(op_id, mm)
+        edit_method().edit_yy(op_id, yy)
+        edit_method().edit_comment(op_id, comment)
 
 
-class Show:
+class show:
 
-    def Show_method(self):
+    def show_method(self):
         i = 0
         while i < len(Operation_History):
             print(" Id you'r operation :"+str((i+1)))
             print(" Sign you'r operation :"+Operation_History[i]['sign'])
-            print(" Summary you'r operation :"+str(Operation_History[i]['summary']))
+            Sum = " Summary you'r operation :"
+            print(Sum+str(Operation_History[i]['summary']))
             print(" Day you'r operation :"+str(Operation_History[i]['dd']))
             print(" Mounth you'r operation :"+str(Operation_History[i]['mm']))
             print(" Year you'r operation :"+str(Operation_History[i]['yy']))
-            print(" Comment to you'r operation :"+Operation_History[i]['comment']+"\n")
-            i+=1
+            Com = " Comment to you'r operation :"
+            print(Com+Operation_History[i]['comment']+"\n")
+            i += 1
